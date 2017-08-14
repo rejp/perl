17feed5f2d22e2e649fde8901bd33efaf3233752
Paweł Bakiera <pawel.bakiera@t-novum.pl>
2017-08-14 22:24:00 +0100

    Czytanie folderu.

--- Projekt_PERL/testowy.pl
+++ Projekt_PERL/testowy.pl
@@ -1,7 +1,13 @@
 #!/usr/bin/perl
-
 use strict;
 use warnings;
 use feature 'say';
 
-say "Welcome!";
+my $path = "C:\\Developer";
+
+opendir(DIR, $path);
+my @content = readdir(DIR);
+foreach (@content) {
+	say $_;
+}
+closedir(DIR);
